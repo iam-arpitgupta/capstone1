22c345fd15eceb0b83c73c0eb51973d44a6c77e1
iam-arpitgupta <thearpitgupta2003@gmail.com>
2025-07-22 12:04:03 +0530

    ci-3 fixed the variable name

--- mlflow_test.py
+++ mlflow_test.py
@@ -4,10 +4,3 @@
 #   mlflow.log_metric('metric name', 1)
 
 
-# this command is req for mac os specific for the ssl verfification 
-import ssl
-import urllib.request
-
-url = "https://www.google.com"
-response = urllib.request.urlopen(url)
-print(response.status)  # should print 200
